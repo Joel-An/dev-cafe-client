f116a246ae0e8d41a6d69b56b1aa89956ed320fd
Joel-An <rejoelve@gmail.com>
2018-12-09 16:12:23 +0900

    ADD : 새로운 레이아웃 추가
    기존 레이아웃은 조금씩 제거 예정

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './Home';
 import Users from './UsersPage';
@@ -8,16 +8,41 @@ import NoMatch from './NoMatch';
 
 import axios from 'axios';
 
+import './App.css';
+
 const App = () => {
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/users" component={Users} />
-          <Route exact path="/posts" component={Posts} />
-          <Route component={NoMatch} />
-        </Switch>
+      <div class="wrapper">
+        <header className="main-head">
+          <Link to="/">Joel's Dev Cafe</Link>
+        </header>
+        <nav class="main-nav">
+          <ul>
+            <li>
+              <a href="">Nav 1</a>
+            </li>
+            <li>
+              <a href="">Nav 2</a>
+            </li>
+            <li>
+              <a href="">Nav 3</a>
+            </li>
+          </ul>
+        </nav>
+        <article class="content">
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/users" component={Users} />
+              <Route exact path="/posts" component={Posts} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </article>
+        <aside class="side">Sidebar</aside>
+        <div class="ad">Advertising</div>
+        <footer class="main-footer">The footer</footer>
       </div>
     </Router>
   );
