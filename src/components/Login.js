ca782fae48b11dbdf49a35e54d3549211e5860d8
Joel-An <rejoelve@gmail.com>
2018-12-10 09:43:51 +0900

    refactor: 로그인 정보 입력창을 컴포넌트로 분리
    사용자에게 정보를 입력받는 기능만 하도록 변경
    API 서버와의 통신은 부모인 Login 컴포넌트가 수행

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,23 +13,7 @@ class Login extends React.Component {
   render() {
     return (
       <div>
-        <form accept-charset="UTF-8" method="post" action="/api/v1/login">
-          <legend>로그인</legend>
-          <div>
-            <label>이메일</label>
-            <input name="email" value="" placeholder="E-mail" type="text" />
-          </div>
-          <div>
-            <label>비밀번호</label>
-            <input
-              name="password"
-              value=""
-              placeholder="Password"
-              type="password"
-            />
-          </div>
-          <input type="submit" value="로그인" />
-        </form>
+        <LoginForm />
         <div>
           <Link to="/signup">회원가입</Link>
         </div>
@@ -40,5 +24,29 @@ class Login extends React.Component {
     );
   }
 }
+
+const LoginForm = () => {
+  return (
+    <div>
+      <form accept-charset="UTF-8" method="post" action="/api/v1/login">
+        <legend>로그인</legend>
+        <div>
+          <label>이메일</label>
+          <input name="email" value="" placeholder="E-mail" type="text" />
+        </div>
+        <div>
+          <label>비밀번호</label>
+          <input
+            name="password"
+            value=""
+            placeholder="Password"
+            type="password"
+          />
+        </div>
+        <input type="submit" value="로그인" />
+      </form>
+    </div>
+  );
+};
 
 export default Login;
