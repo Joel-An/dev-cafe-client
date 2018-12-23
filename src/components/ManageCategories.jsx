c4b5d3c640714ac5ae084649d74b408354a96d55
Joel-An <rejoelve@gmail.com>
2018-12-23 22:00:52 +0900

    feat: 카테고리 목록 표시 기능 추가

--- src/components/ManageCategories.jsx
+++ src/components/ManageCategories.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import CreateCategory from './CreateCategory';
 
@@ -11,10 +12,33 @@ class ManageCategories extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.getCategories();
+  }
+
+  getCategories = () => {
+    axios
+      .get('/api/v1/categories')
+      .then((result) => {
+        const { categories } = result.data;
+        this.setState({ categories });
+      })
+      .catch(err => console.error(err.message));
+  }
+
   render() {
     const { token } = this.props;
+    const { categories } = this.state;
+    const style = { border: '0.5px solid #dddddd' };
     return (
       <div >
+        <ul style={style}>
+          {categories
+            && categories.map(category => (
+              <li style={style} id={category._id}>{category.name}</li>
+            ))
+          }
+        </ul>
         <CreateCategory token={token} />
       </div>
     );
