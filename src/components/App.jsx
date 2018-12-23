05cfd0984c6147b7244cf288289c54f26b0985ba
Joel-An <rejoelve@gmail.com>
2018-12-23 16:25:50 +0900

    refactor: App.jsx 클래스 컴포넌트로 변경
    token이 필요할 때 localStorage에서 가져오는 건 단방향 데이터 흐름에 어긋나서
    App 컴포넌트에서 token을 관리하고려함

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,45 +13,58 @@ import Header from './layout/Header';
 
 import './App.css';
 
-const App = () => (
-  <Router>
-    <div className="wrapper">
-      <Header />
-      <nav className="main-nav">
-        <ul>
-          <li>
-            <Link to="/users">USERS</Link>
-          </li>
-          <li>
-            <Link to="/posts">POSTS</Link>
-          </li>
-          <li>
-            <Link to="/login">LOGIN</Link>
-          </li>
-          <li>
-            <Link to="/signup">SIGNUP</Link>
-          </li>
-        </ul>
-      </nav>
-      <article className="content">
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/users" component={Users} />
-            <Route exact path="/posts" component={Posts} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route component={NoMatch} />
-          </Switch>
-        </div>
-      </article>
-      <aside className="side">Sidebar</aside>
-      <div className="ad">Advertising</div>
-      <footer className="main-footer">
-        <p>Would you like a cup of coffee?</p>
-      </footer>
-    </div>
-  </Router>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      token: localStorage.getItem('token'),
+    };
+  }
+
+  updateToken = () => {
+    this.setState({ token: localStorage.getItem('token') });
+  }
+
+  render() {
+    return (<Router>
+      <div className="wrapper">
+        <Header />
+        <nav className="main-nav">
+          <ul>
+            <li>
+              <Link to="/users">USERS</Link>
+            </li>
+            <li>
+              <Link to="/posts">POSTS</Link>
+            </li>
+            <li>
+              <Link to="/login">LOGIN</Link>
+            </li>
+            <li>
+              <Link to="/signup">SIGNUP</Link>
+            </li>
+          </ul>
+        </nav>
+        <article className="content">
+          <div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/users" component={Users} />
+              <Route exact path="/posts" component={Posts} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </article>
+        <aside className="side">Sidebar</aside>
+        <div className="ad">Advertising</div>
+        <footer className="main-footer">
+          <p>Would you like a cup of coffee?</p>
+        </footer>
+      </div>
+    </Router>);
+  }
+}
 export default App;
