6326e11ae3874ef6377566fea87118b5a3759101
Joel-An <rejoelve@gmail.com>
2019-01-04 01:09:21 +0900

    refactor: 함수형 컴포넌트로 변경

--- src/components/categories/ManageCategories.jsx
+++ src/components/categories/ManageCategories.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import CreateCategory from './CreateCategory';
 import Categories from './Categories';
 
 
-class ManageCategories extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-    const { token } = this.props;
-    return (
-      <div >
-        <Categories token={token} />
-        <CreateCategory token={token} />
-      </div>
-    );
-  }
-}
-
-ManageCategories.propTypes = {
-  token: PropTypes.string.isRequired,
-};
-
+const ManageCategories = () => (
+  <div >
+    <Categories />
+    <CreateCategory />
+  </div>
+);
 
 export default ManageCategories;
