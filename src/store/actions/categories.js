d05f50f02057068c71511fb48ef4a029e354666b
Joel-An <rejoelve@gmail.com>
2019-01-03 16:43:56 +0900

    refactor: 레거시 코드 제거 (fetch categories *)

--- src/store/actions/categories.js
+++ src/store/actions/categories.js
@@ -1,9 +1,5 @@
 import {
-  FETCH_CATEGORIES,
-  FETCH_CATEGORIES_FULFILLED,
-  FETCH_CATEGORIES_REJECTED,
   LOAD_CATEGORIES,
-  LOAD_CATEGORIES_SUCCESS,
   CATEGORIES_REQUEST,
   CATEGORIES_SUCCESS,
   CATEGORIES_FAILURE,
@@ -12,10 +8,6 @@ import {
 import { CALL_API, Schemas } from '../sagas/apiSaga';
 
 export const fetchCategories = () => ({
-  type: FETCH_CATEGORIES,
-});
-
-export const newFetchCategoires = () => ({
   type: CALL_API,
   types: [CATEGORIES_REQUEST, CATEGORIES_SUCCESS, CATEGORIES_FAILURE],
   endpoint: '/categories',
@@ -23,22 +15,6 @@ export const newFetchCategoires = () => ({
   schema: Schemas.CATEGORY_ARRAY,
 });
 
-export const fetchCategoriesFulfilled = categories => ({
-  type: FETCH_CATEGORIES_FULFILLED,
-  payload: categories,
-});
-
-export const fetchCategoriesRejected = error => ({
-  type: FETCH_CATEGORIES_REJECTED,
-  error,
-});
-
-
 export const loadCategories = () => ({
   type: LOAD_CATEGORIES,
-});
-
-export const loadCategoriesSuccess = categories => ({
-  type: LOAD_CATEGORIES_SUCCESS,
-  payload: categories,
 });
--- src/store/sagas/categories.js
+++ src/store/sagas/categories.js
@@ -1,31 +1,20 @@
 import {
-  put, takeLatest, call, select,
+  put, takeLatest, select,
 } from 'redux-saga/effects';
 
-import { LOAD_CATEGORIES, FETCH_CATEGORIES } from '../types/categories';
+import { LOAD_CATEGORIES } from '../types/categories';
 import * as actions from '../actions/categories';
-import * as api from '../../api/categories';
 
-function* fetchCategoriesSaga() {
-  try {
-    const { data } = yield call(api.getCategories);
-    yield put(actions.fetchCategoriesFulfilled(data.categories));
-  } catch (error) {
-    yield put(actions.fetchCategoriesRejected(error.response));
-  }
-}
 
 function* loadCategoriesSaga() {
   const state = yield select();
-  if (state.categories.payload.length) {
+  if (Object.keys(state.entities.categories).length) {
     yield put(actions.loadCategoriesSuccess());
   } else {
-    yield put(actions.newFetchCategoires());
     yield put(actions.fetchCategories());
   }
 }
 
 export default function* watchCategories() {
   yield takeLatest(LOAD_CATEGORIES, loadCategoriesSaga);
-  yield takeLatest(FETCH_CATEGORIES, fetchCategoriesSaga);
 }
