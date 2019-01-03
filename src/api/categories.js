3b493350f34d53118e8916eda51db2eb6b7b75e0
Joel-An <rejoelve@gmail.com>
2019-01-04 00:09:16 +0900

    refactor: postCategory 추상화
    굳이 세부 로직을 컴포넌트에 작성할 필요가 없어서 따로 추출함

--- src/api/categories.js
+++ src/api/categories.js
@@ -1,3 +1,9 @@
 import axios from 'axios';
 
 export const getCategories = () => axios.get('api/v1/categories');
+
+export const postCategory = (category, token) => {
+  const config = { headers: { 'x-access-token': token } };
+
+  return axios.post('/api/v1/categories', category, config);
+};
