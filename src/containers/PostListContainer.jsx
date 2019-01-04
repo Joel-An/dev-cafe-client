470129e9df98630d5bf46953ed14798c2e2a4b16
Joel-An <rejoelve@gmail.com>
2019-01-04 21:44:47 +0900

    feat: 카테고리별 글 목록을 받아오는 PostListContainer 추가

---
+++ src/containers/PostListContainer.jsx
@@ -0,0 +1,48 @@
+import { connect } from 'react-redux';
+import React from 'react';
+
+import { loadPosts as loadPostsAction } from '../store/actions/posts';
+
+export default function withPostListContainer(ComposedComponent) {
+  class PostListContainer extends React.Component {
+    componentDidMount() {
+      const { category, loadPosts } = this.props;
+
+      loadPosts(category);
+    }
+
+    componentWillReceiveProps(nextProps) {
+      const { category, loadPosts } = this.props;
+      if (category !== nextProps.category) {
+        loadPosts(nextProps.category);
+      }
+    }
+
+    render() {
+      const newProps = { ...this.props };
+      delete newProps.loadPosts;
+
+      return (
+        <ComposedComponent {...newProps} />
+      );
+    }
+  }
+
+  const getCategoryId = (query) => {
+    const categoryId = query.slice(query.indexOf('=') + 1);
+    return categoryId;
+  };
+
+  const mapStateToProps = (state, ownProps) => {
+    const { posts, categories } = state.entities;
+    const { search: query } = ownProps.location;
+
+    const category = getCategoryId(query);
+
+    return { posts, categories, category };
+  };
+  const mapDispatchToProps = { loadPosts: loadPostsAction };
+
+
+  return connect(mapStateToProps, mapDispatchToProps)(PostListContainer);
+}
