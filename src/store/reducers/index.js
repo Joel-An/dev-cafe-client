ce3ada18ff339b5f2f9a54ff2c9fa407b6587a7f
Joel-An <rejoelve@gmail.com>
2019-01-04 21:40:58 +0900

    feat: 카테고리별 글목록 캐시를 위한 reducer 추가
    store.state.pagination.postsByCategory[categoryId] 이렇게 접근 가능

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import merge from 'lodash/merge';
 import auth from './auth';
+import paginate from './paginate';
+import * as PostActions from '../types/posts';
 
 const entities = (state = { categories: {}, users: {} }, action) => {
   if (action.response && action.response.entities) {
@@ -13,6 +15,17 @@ const entities = (state = { categories: {}, users: {} }, action) => {
   return state;
 };
 
+const pagination = combineReducers({
+  postsByCategory: paginate({
+    mapActionToKey: action => action.category,
+    types: [
+      PostActions.GET_POSTS_REQUEST,
+      PostActions.GET_POSTS_SUCCESS,
+      PostActions.GET_POSTS_FAILURE,
+    ],
+  }),
+});
+
 export default combineReducers({
-  auth, entities,
+  auth, entities, pagination,
 });
