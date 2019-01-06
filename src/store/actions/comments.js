f1b89414bbb7c1bfc1ba5db34aff4e5e14c7bfd5
Joel-An <rejoelve@gmail.com>
2019-01-07 03:53:13 +0900

    fix: 잘못된 endpoint 수정, loadComments() 누락된 파라메터 추가

--- src/store/actions/comments.js
+++ src/store/actions/comments.js
@@ -11,15 +11,16 @@ import { CALL_API, Schemas } from '../sagas/apiSaga';
 export const fetchComments = (postId, refreshCache = false) => ({
   type: CALL_API,
   types: [GET_COMMENTS_REQUEST, GET_COMMENTS_SUCCESS, GET_COMMENTS_FAILURE],
-  endpoint: `/comments/post=${postId}`,
+  endpoint: `/comments?post=${postId}`,
   postId,
   method: 'get',
   refreshCache,
   schema: Schemas.COMMENT_ARRAY,
 });
 
-export const loadComments = () => ({
+export const loadComments = postId => ({
   type: LOAD_COMMENTS,
+  postId,
 });
 
 export const loadCommentsSuccess = () => ({
