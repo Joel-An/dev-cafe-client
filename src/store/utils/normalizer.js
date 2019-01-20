c7effe6f0a917281153c0f4bf0b13fb27f303a95
Joel-An <rejoelve@gmail.com>
2019-01-20 11:29:23 +0900

    feat: normalizeCategories 함수 생성
    굳이 스키마에 대해서 알릴 필요가 없는 거 같아서 추상화함

--- src/store/utils/normalizer.js
+++ src/store/utils/normalizer.js
@@ -1,5 +1,12 @@
 import { normalize } from 'normalizr';
 
+import * as Schema from './schema';
+
 const normalizeData = (data, schema) => normalize(data, schema);
+
+export const normalizeCategories = (categories) => {
+  const data = Array.isArray(categories) ? categories : [categories];
+  return normalizeData(data, [Schema.categorySchema]);
+};
 
 export default normalizeData;
