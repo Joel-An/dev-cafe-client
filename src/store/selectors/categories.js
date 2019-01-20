c230ff10a6af46816daf19076613a8713231c4c9
Joel-An <rejoelve@gmail.com>
2019-01-20 23:37:36 +0900

    feat: categorySelector 기준을 name에서 id로 변경

--- src/store/selectors/categories.js
+++ src/store/selectors/categories.js
@@ -3,12 +3,12 @@ import { createSelector } from 'reselect';
 export const getCategories = state => state.newEntities.categories;
 // TODO: newEntities -> entities 변경
 
-const getCategoryNames = createSelector(
+const getCategoryIds = createSelector(
   getCategories,
   categories => Object.keys(categories),
 );
 
-export const getParentCategoryNames = createSelector(
-  [getCategories, getCategoryNames],
-  (categories, categoryNames) => categoryNames.filter(name => !categories[name].isChild),
+export const getParentCategoryIds = createSelector(
+  [getCategories, getCategoryIds],
+  (categories, categoryIds) => categoryIds.filter(id => !categories[id].isChild),
 );
