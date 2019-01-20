4a3812dbdc2bc2df9a7c58fe051131f64ee14177
Joel-An <rejoelve@gmail.com>
2019-01-20 21:14:35 +0900

    refactor: 카테고리 셀렉터 path 변경
    새로운 리듀서를 사용하기 전까지 state.newEntities.categories로 사용할 예정

--- src/store/selectors/categories.js
+++ src/store/selectors/categories.js
@@ -1,6 +1,7 @@
 import { createSelector } from 'reselect';
 
-export const getCategories = state => state.entities.categories;
+export const getCategories = state => state.newEntities.categories;
+// TODO: newEntities -> entities 변경
 
 const getCategoryNames = createSelector(
   getCategories,
