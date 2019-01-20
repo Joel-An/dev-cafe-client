fe9149d1e53ef6159abcb85a805f314c9c889cfd
Joel-An <rejoelve@gmail.com>
2019-01-20 23:19:22 +0900

    feat: normalizer 모듈화

--- src/store/utils/normalizer.js
+++ src/store/utils/normalizer.js
@@ -2,14 +2,19 @@ import { normalize } from 'normalizr';
 
 import * as Schema from './schema';
 
-const normalizeData = (data, schema) => normalize(data, schema);
+const normalizeData = (data, schema) => {
+  const normalizedData = normalize(data, schema);
+
+  normalizedData.getEntity = entityName => normalizedData.entities[entityName];
+  normalizedData.hasEntity = entityName => (!!normalizedData.entities[entityName]);
+  return normalizedData;
+};
 
 export const normalizeCategories = (categories) => {
   const data = Array.isArray(categories) ? categories : [categories];
 
   const result = normalizeData(data, [Schema.categorySchema]);
-
-  result.selectCategories = () => result.entities.categories;
+  result.selectCategories = () => result.getEntity('categories');
   return result;
 };
 
