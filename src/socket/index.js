4e9d732c80c521127b3a593accaf5fe877c080c4
Joel-An <rejoelve@gmail.com>
2019-01-25 01:13:36 +0900

    feat: socket.io uri 설정 변경
    지금 당장은 uri를 설정할 필요가 없음
    3S + EC2 서버로 분리되면 설정할 예정

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,11 +2,14 @@ import openSocket from 'socket.io-client';
 
 import { getCategory, removeCategory } from '../store/actions/categories';
 
-const setUri = () => (process.env.NODE_ENV === 'development' ? '/socket.io' : 'http://rejoelve.com');
-
-const connectSocket = URI => openSocket(URI, { transports: ['websocket'] });
+const connectSocket = () => openSocket({ transports: ['websocket'] });
 
 const setHandler = (socket, store) => {
+  socket.on('connect', () => {
+    // eslint-disable-next-line no-console
+    console.log('socket.io is connected');
+  });
+
   socket.on('NEW_CATEGORY', (id) => {
     store.dispatch(getCategory(id));
   });
@@ -17,9 +20,7 @@ const setHandler = (socket, store) => {
 };
 
 const configureSocket = (store) => {
-  const URI = setUri();
-
-  const socket = connectSocket(URI);
+  const socket = connectSocket();
   setHandler(socket, store);
 
   const closeSocket = () => socket.close();
