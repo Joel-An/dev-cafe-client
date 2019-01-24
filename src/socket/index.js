060f8ba653df01431d3653f507d4905e4ea3c9a6
Joel-An <rejoelve@gmail.com>
2019-01-24 14:57:28 +0900

    refactor: setUri 테스트 모드 제거

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,20 +2,9 @@ import openSocket from 'socket.io-client';
 
 import { getCategory, removeCategory } from '../store/actions/categories';
 
-const setUri = () => {
-  switch (process.env.NODE_ENV) {
-  case 'development':
-    return '/socket.io';
-  case 'production':
-    return 'http://rejoelve.com';
-  case 'test':
-    return 'http://localhost';
-  default:
-    throw new Error('NODE_ENV 설정이 잘못되었습니다.');
-  }
-};
+const setUri = () => (process.env.NODE_ENV === 'development' ? '/socket.io' : 'http://rejoelve.com');
 
-const connectSocket = URI => openSocket({ path: URI, transports: ['websocket'] });
+const connectSocket = URI => openSocket(URI, { transports: ['websocket'] });
 
 const setHandler = (socket, store) => {
   socket.on('NEW_CATEGORY', (id) => {
