9707f13da068c9c23c7b52dbdbfdc2eea3279996
Joel-An <rejoelve@gmail.com>
2019-02-02 02:29:23 +0900

    fix: 카테고리 fetch 완료전에 참조하는 오류 수정
    이슈 #1과 관련있음. 임시방편이긴 하지만 Category를 출력할 때, 정보가 없다면 Loading 메시지를 출력하도록 변경함
    
    미봉책이긴 하지만 이거보다 나은 방법을 찾긴 힘들 것 같다.

--- src/components/categories/Category.jsx
+++ src/components/categories/Category.jsx
@@ -4,6 +4,10 @@ import withCategoryContainer from '../../containers/CategoryContainer';
 const Category = (props) => {
   const { category, renderCategory } = props;
 
+  if (!category) {
+    return <p>Loading Category...</p>;
+  }
+
   return renderCategory(category);
 };
 
