e0fd9806b794ae48173ef8aa62244eeaffe9cc95
Joel-An <rejoelve@gmail.com>
2019-02-04 04:15:49 +0900

    feat: editingComment 셀렉터 추가

--- src/store/selectors/comments.js
+++ src/store/selectors/comments.js
@@ -4,3 +4,5 @@ export const selectComments = state => state.newEntities.comments;
 export const selectCommentById = (state, id) => selectComments(state)[id];
 
 export const selectCommentsMetaByPost = (state, postId) => state.newPagination.commentsByPost[postId];
+
+export const selectEditingCommentById = (state, id) => state.editing.comments[id];
