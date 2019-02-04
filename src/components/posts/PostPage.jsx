da0f9495e09f4baead8036eb80a2c06b4a3ccbaa
Joel-An <rejoelve@gmail.com>
2019-02-04 20:12:14 +0900

    feat: PostView 컴포넌트 생성, PostPage에 적용
    post 삭제 버튼을 만들 때 post.author 정보가 필요함.

--- src/components/posts/PostPage.jsx
+++ src/components/posts/PostPage.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
 
-import Post from './Post';
-import Category from '../categories/Category';
-import User from '../users/User';
+import PostView from './PostView';
 import CommentArea from '../comments/CommentArea';
-
-const renderPost = (post) => {
-  if (!post) {
-    return (<p>글이 없어!</p>);
-  }
-
-  const renderCategory = category => (
-    <p>카테고리 : {category.name}</p>
-  );
-  const renderUser = user => (
-    <p>작성자 : {user.profileName}</p>
-  );
-
-  return (
-    <p>
-      제목 : {post.title}<br/>
-      <Category categoryId={post.category} renderCategory={renderCategory}/>
-      <User userId={post.author} renderUser={renderUser}/>
-      내용 : {post.contents || 'Loading...'}
-    </p>
-  );
-};
 
 const PostPage = (props) => {
   const { match } = props;
@@ -33,7 +9,7 @@ const PostPage = (props) => {
 
   return (
     <div>
-      <Post postId={postId} renderPost={renderPost}/>
+      <PostView postId={postId}/>
       <CommentArea
         postId={postId}
       />
---
+++ src/components/posts/PostView.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import withPostContainer from '../../containers/PostContainer';
+import withMyInfoContainer from '../../containers/MyInfoContainer';
+
+import Category from '../categories/Category';
+import User from '../users/User';
+import DeletePost from './DeletePost';
+
+const renderCategory = category => (
+  <p>카테고리 : {category.name}</p>
+);
+const renderUser = user => (
+  <p>작성자 : {user.profileName}</p>
+);
+
+const PostView = (props) => {
+  const { post, myInfo } = props;
+
+  if (!post) {
+    return (<p>글이 없어!</p>);
+  }
+
+  const isMyPost = (post.author === myInfo._id);
+
+  return (
+    <p>
+      제목 : {post.title}<br/>
+      <Category categoryId={post.category} renderCategory={renderCategory}/>
+      <User userId={post.author} renderUser={renderUser}/>
+      내용 : {post.contents || 'Loading...'}
+      {isMyPost && <DeletePost postId={post._id} categoryId={post.category}/>}
+    </p>
+  );
+};
+
+const PostViewWithMyInfo = withMyInfoContainer(PostView);
+
+export default withPostContainer(PostViewWithMyInfo);
