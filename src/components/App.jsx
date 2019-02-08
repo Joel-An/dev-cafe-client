16ae0972ba03a6b9e0e6ebd27b040d4182056eec
Joel-An <rejoelve@gmail.com>
2019-02-08 19:16:27 +0900

    feat: 사용하지않는 state삭제, 함수형 컴포넌트로 변경

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Switch, BrowserRouter as Router, Route, Link,
+  Switch, BrowserRouter as Router, Route,
 } from 'react-router-dom';
 
 import Home from './Home';
@@ -20,55 +20,36 @@ import EditPostPage from './posts/EditPostPage';
 import CategoryMenu from './categories/CategoryMenu';
 import AdminPage from './AdminPage';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => (
+  <Router>
+    <div className="wrapper">
+      <Header/>
+      <article className="content">
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/users" component={Users} />
+            <Route exact path="/oldPosts" component={OldPosts} />
+            <Route path="/posts/:id" component={PostPage} />
+            <Route exact path="/posts" component={PostListPage} />
+            <Route path="/write/:categoryId" component={Write} />
+            <Route path="/edit/:id" component={EditPostPage}/>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/categoryManager" component={CategoryManager} />
+            <Route exact path="/admin" component={AdminPage} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </article>
+      <aside className="side">
+        <CategoryMenu/>
+      </aside>
+      <footer className="main-footer">
+        <p>Would you like a cup of coffee?</p>
+      </footer>
+    </div>
+  </Router>
+);
 
-    this.state = {
-      token: localStorage.getItem('token'),
-    };
-  }
-
-  updateToken = () => {
-    this.setState({ token: localStorage.getItem('token') });
-  }
-
-  logout = () => {
-    localStorage.removeItem('token');
-    this.updateToken();
-  }
-
-  render() {
-    const { token } = this.state;
-    return (<Router>
-      <div className="wrapper">
-        <Header/>
-        <article className="content">
-          <div>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/users" component={Users} />
-              <Route exact path="/oldPosts" component={OldPosts} />
-              <Route path="/posts/:id" render={props => (<PostPage {...props} token={token}/>)} />
-              <Route exact path="/posts" component={PostListPage} />
-              <Route path="/write/:categoryId" render={props => (<Write {...props} token={token}/>)} />
-              <Route path="/edit/:id" component={EditPostPage}/>
-              <Route exact path="/login" component={Login}/>
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/categoryManager" component={CategoryManager} />
-              <Route exact path="/admin" component={AdminPage} />
-              <Route component={NoMatch} />
-            </Switch>
-          </div>
-        </article>
-        <aside className="side">
-          <CategoryMenu/>
-        </aside>
-        <footer className="main-footer">
-          <p>Would you like a cup of coffee?</p>
-        </footer>
-      </div>
-    </Router>);
-  }
-}
 export default App;
