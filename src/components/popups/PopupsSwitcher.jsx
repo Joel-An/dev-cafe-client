bbd301468016d5ea05f9af9032c2d2e3b7590d2f
Joel-An <rejoelve@gmail.com>
2019-02-12 21:00:18 +0900

    feat: 열린 팝업들을 선택해 렌더링하는 PopupSwitcher 생성
    둘 이상의 팝업이 동시에 뜰 수 있기 때문에 열린 팝업들을 관리하는 PopupSwitcher를 만들었다.
    
    키보드(esc키)로 닫을 수 '없는' 팝업이 존재하고, 키 입력시 최상위 팝업 하나만 닫혀야하기 때문에
    팝업들을 선택적으로 렌더링하는 PopupSwitcher가 키보드 이벤트 리스너를 관리한다.
    
    기본 책임은 리덕스 스토어에서 열린 팝업에 대한 정보를 받은 뒤,
    children으로 등록된 모든 팝업중 열린 팝업만 렌더링 한다.
    
    + esc키 입력 시 최신 팝업을 닫는다 ( 키 입력이 허용되지 않은 팝업은 닫지 않음 )

---
+++ src/components/popups/PopupsSwitcher.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { closePopup as closePopupAction } from '../../store/actions/popups';
+
+import './PopupsSwitcher.scss';
+
+class PopupSwitcher extends React.Component {
+  componentDidMount() {
+    document.body.addEventListener('keydown', this.closeLastPopup);
+  }
+
+  componentWillUnmount() {
+    document.body.removeEventListener('keydown', this.closeLastPopup);
+  }
+
+  getAllPopups = () => {
+    const { children } = this.props;
+
+    return React.Children.toArray(children);
+  }
+
+  getVisiblePopups = () => {
+    const { openedPopups, closePopup } = this.props;
+
+    const allPopups = this.getAllPopups();
+
+    const visiblePopups = openedPopups.map((entry) => {
+      const { popupType, popupProps } = entry;
+
+      const Element = allPopups.find(popup => popup.props.popupType === popupType);
+
+      return React.cloneElement(Element, {
+        close: () => closePopup(popupType),
+        ...popupProps,
+      });
+    });
+
+    return visiblePopups;
+  }
+
+  getLastPopup = () => {
+    const { openedPopups } = this.props;
+    const lastPopupInfo = openedPopups[openedPopups.length - 1];
+
+    const { popupType: lastPopupType } = lastPopupInfo;
+
+    const allPopups = this.getAllPopups();
+    const lastPopup = allPopups.find(popup => popup.props.popupType === lastPopupType);
+
+    return lastPopup;
+  }
+
+  closeLastPopup = (e) => {
+    if ((e.key === 'Escape' || e.keyCode === 27)) {
+      const lastPopup = this.getLastPopup();
+      this.closeIfEscapable(lastPopup)();
+    }
+  }
+
+  closeIfEscapable = popup => () => {
+    const { closePopup } = this.props;
+
+    if (popup && popup.props.escapable) {
+      closePopup(popup.props.popupType);
+    }
+  }
+
+  render() {
+    const popups = this.getVisiblePopups();
+
+    if (popups.length === 0) { return null; }
+
+    return (
+      <div className="PopupSwitcher">
+        {popups.map(popup => (
+          <div
+            key={popup.props.popupType}
+            className="popup-background"
+            onClick={this.closeIfEscapable(popup)}
+          >
+            {popup}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const openedPopups = state.popups;
+
+  return { openedPopups };
+};
+
+const mapDispatchToProps = { closePopup: closePopupAction };
+
+export default connect(mapStateToProps, mapDispatchToProps)(PopupSwitcher);
