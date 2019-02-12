77990f36817739e310c10ac73ca0bf0e4146308b
Joel-An <rejoelve@gmail.com>
2019-02-13 01:45:31 +0900

    feat: confirm 팝업을 띄우는 Confirm Button 컴포넌트 추가

---
+++ src/components/popups/buttons/ConfirmButton.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { openConfirm as openConfirmAction } from '../../../store/actions/popups';
+
+class ConfirmButton extends React.Component {
+  openPopup = () => {
+    const { openConfirm, onConfirm, message } = this.props;
+    const {
+      left, right, top,
+    } = this.button.getBoundingClientRect();
+
+    openConfirm({
+      pos: {
+        left, right, top,
+      },
+      onConfirm,
+      message,
+    });
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <button type="button" onClick={this.openPopup} ref={ (el) => { this.button = el; }}>
+        {children}
+      </button>
+    );
+  }
+}
+
+const mapDispatchToProps = { openConfirm: openConfirmAction };
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(ConfirmButton);
