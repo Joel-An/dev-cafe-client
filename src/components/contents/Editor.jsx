4414e0d9e031de2b04261177c907453cd744dc1e
Joel-An <rejoelve@gmail.com>
2019-02-16 07:16:08 +0900

    feat: 업로드된 이미지 링크를 삽입하기 위해 Editor 코드 변경
    끝에다가 넣으면 간단한데 중간에 넣을 수 있게 만들었음

--- src/components/contents/Editor.jsx
+++ src/components/contents/Editor.jsx
@@ -3,29 +3,61 @@ import { Controlled as CodeMirror } from 'react-codemirror2';
 
 import 'codemirror/lib/codemirror.css';
 
-class Editor extends React.PureComponent {
-  render() {
-    const { onChange, contents, autofocus } = this.props;
-    return (
-      <CodeMirror
-        value={contents}
-        options={{
-          mode: {
-            name: 'markdown',
-            highlightFormatting: true,
-          },
-          viewportMargin: Infinity,
-          autofocus,
-        }}
-        onBeforeChange={(editor, data, value) => {
-          onChange(value);
-        }}
-        onChange={(editor, data, value) => {
-        }}
-        editorDidMount={e => e.setSize(null, '100%')}
-      />
-    );
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.editor = null;
   }
+
+  componentDidUpdate(prevProps) {
+    const { insertText } = this.props;
+    if (!prevProps.insertText && insertText) {
+      this.insertTextAtCursor();
+    }
+  }
+
+  insertTextAtCursor = () => {
+    const { insertText, clearInsertText } = this.props;
+    const selection = this.editor.getSelection();
+
+    if (selection.length > 0) {
+      this.editor.replaceSelection(insertText);
+    } else {
+      const doc = this.editor.getDoc();
+      const cursor = doc.getCursor();
+      doc.replaceRange(insertText, cursor);
+    }
+
+    clearInsertText();
+  }
+
+   editorDidMount = (editor) => {
+     this.editor = editor;
+     this.editor.setSize(null, 'auto');
+   }
+
+   render() {
+     const { onChange, contents, autofocus } = this.props;
+     return (
+       <CodeMirror
+         value={contents}
+         options={{
+           mode: {
+             name: 'markdown',
+             highlightFormatting: true,
+           },
+           viewportMargin: Infinity,
+           autofocus,
+         }}
+         onBeforeChange={(editor, data, value) => {
+           onChange(value);
+         }}
+         onChange={(editor, data, value) => {
+         }}
+         editorDidMount={this.editorDidMount}
+       />
+     );
+   }
 }
 
 export default Editor;
