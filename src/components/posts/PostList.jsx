aeb48d32a0c1b2ab737c6d06a12b83195dd76a48
Joel-An <rejoelve@gmail.com>
2019-02-17 08:53:18 +0900

    feat: 글 목록에 fetch type 별로 LoadingSpinner 추가, 무한 스크롤 구현
    새로운 글 하나를 fetch할 때는 목록 위쪽에,
    이전 글을 불러올 때는 목록 아래쪽에 LoadingSpinner가 생성되도록 변경함.
    
    무한 스크롤을 구현하여 스크롤 만으로 마지막 글까지 fetch할 수 있도록 변경함
    
    + 편의를 위해서 페이지 버튼도 추가할 예정

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -1,30 +1,89 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import withPostListContainer from '../../containers/PostListContainer';
+import { connectComponent } from '../../utils';
+
+import { fetchNextPagePosts as fetchNextPagePostsAction } from '../../store/actions/posts';
+
 
 import PostListItem from './PostListItem';
 import LoadingSpinner from '../loadingSpinner/LoadingSpinner';
 
-const PostList = (props) => {
-  const { postsMeta } = props;
-
-  if (!postsMeta || (postsMeta.isFetching && postsMeta.ids.length === 0)) {
-    return <LoadingSpinner/>;
+class PostList extends React.Component {
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll, false);
   }
 
-  if (postsMeta.ids.length === 0) {
-    return <p>글이 없어!</p>;
+  componentDidUpdate() {
+    const { postsMeta } = this.props;
+    const { scrollHeight, offsetHeight } = document.body;
+
+    if (scrollHeight === offsetHeight) {
+      // 스크롤 바가 없을 때는 스크롤 이벤트가 발생하지 않는다.
+
+      const hasNextPageUrl = postsMeta && !postsMeta.isFetchingPosts && postsMeta.nextPageUrl;
+      if (hasNextPageUrl) {
+        // fetch가 완료되었는데도, nextPageUrl이 있다면 fetch한다.
+        this.fetchNextPage();
+      }
+    }
   }
 
-  return (
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll, false);
+  }
+
+  fetchNextPage = () => {
+    const { postsMeta, fetchNextPagePosts, categoryId } = this.props;
+    fetchNextPagePosts(postsMeta.nextPageUrl, categoryId);
+  }
+
+  onScroll = () => {
+    const { scrollHeight, offsetHeight } = document.body;
+    const { scrollY } = window;
+    if (scrollHeight - (scrollY + offsetHeight) < 500) {
+      const { postsMeta } = this.props;
+      if (postsMeta && !postsMeta.isFetchingPosts && postsMeta.nextPageUrl) {
+        this.fetchNextPage();
+      }
+    }
+  }
+
+  render() {
+    const { postsMeta, fetchNextPagePosts, categoryId } = this.props;
+
+    if (!postsMeta || (postsMeta.isFetchingPosts && postsMeta.ids.length === 0)) {
+      return <LoadingSpinner/>;
+    }
+
+    if (!postsMeta.isFetchingPosts && (postsMeta.ids.length === 0)) {
+      return <p>글이 없어!</p>;
+    }
+
+    return (
+    <>
     <ul className="postList" >
-      {postsMeta.isFetching && <LoadingSpinner key="isLoading"/>}
+      {postsMeta.isFetchingNewPost && <LoadingSpinner key="fetchingNewPost"/>}
       {postsMeta.ids.map(postId => (
         <PostListItem postId={postId} key={postId}/>
       ))
       }
+      {postsMeta.isFetchingPosts && <LoadingSpinner key="fetchingPosts"/>}
     </ul>
-  );
-};
+    { !!postsMeta.nextPageUrl || <span>
+      이젠 끝이다!
+    </span>
+    }
+    </>
+    );
+  }
+}
 
-export default withPostListContainer(PostList);
+const mapDispatchToProps = { fetchNextPagePosts: fetchNextPagePostsAction };
+
+export default connectComponent(PostList,
+  [
+    connect(null, mapDispatchToProps),
+    withPostListContainer,
+  ]);
