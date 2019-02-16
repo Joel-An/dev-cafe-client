834a94c6bfc28c4a98fcd9272b9c0514452e0018
Joel-An <rejoelve@gmail.com>
2019-02-16 10:57:13 +0900

    feat: 관리자 페이지 접근 시 경고 출력

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const AdminPage = () => (
-  <Link to="/categoryManager">MANAGE CATEGORIES</Link>
+import withAlertContainer from '../containers/AlertContainer';
+import withMyInfoContainer from '../containers/MyInfoContainer';
 
-);
+import { connectComponent } from '../utils';
 
-export default AdminPage;
+const AdminPage = (props) => {
+  const { myInfo, openAlert } = props;
+
+  if (!myInfo || !myInfo.isAdmin) {
+    openAlert({
+      title: 'Warning!!',
+      message: '관리자가 아니라면 접근할 수 없...지 않아요',
+    });
+  }
+  return (
+    <Link to="/categoryManager">MANAGE CATEGORIES</Link>
+  );
+};
+
+export default connectComponent(AdminPage,
+  [
+    withAlertContainer,
+    withMyInfoContainer,
+  ]);
