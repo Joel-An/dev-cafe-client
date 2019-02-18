0c4cb6c5c08d76f607b66740039627ecb2e21965
Joel-An <rejoelve@gmail.com>
2019-02-18 15:49:00 +0900

    fix: style property 이름 수정
    리액트에서는 이렇게 써야한다고함

--- src/components/loadingSpinner/LoadingSpinner.jsx
+++ src/components/loadingSpinner/LoadingSpinner.jsx
@@ -4,7 +4,7 @@ import './LoadingSpinner.scss';
 
 const LoadingSpinner = (props) => {
   const { center } = props;
-  const style = center ? { 'justify-content': 'center' } : {};
+  const style = center ? { justifyContent: 'center' } : {};
 
   return (
     <div className="loading-spinner-container" style={style}>
