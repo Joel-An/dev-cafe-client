280d5db300b97a51785e1ffed1bc894197cb8112
Joel-An <rejoelve@gmail.com>
2019-02-18 17:42:09 +0900

    feat: 댓글 목록 무한 스크롤 구현

--- src/components/comments/CommentList.jsx
+++ src/components/comments/CommentList.jsx
@@ -8,21 +8,47 @@ import { connectComponent } from '../../utils';
 
 import { fetchNextPageComments as fetchNextPageCommentsAction } from '../../store/actions/comments';
 
-const CommentList = (props) => {
-  const { commentsMeta } = props;
-
-  if (!commentsMeta || (commentsMeta.isFetchingComments && commentsMeta.ids.length === 0)) {
-    return <LoadingSpinner center/>;
+class CommentList extends React.Component {
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll, false);
   }
 
-  return (
-    <ul className="CommentList">
-      {commentsMeta.ids.map(id => <CommentListItem commentId={id} key={id}/>)}
-      {commentsMeta.isFetchingComments && <LoadingSpinner key="isFetchingComments" center/>}
-      {commentsMeta.isFetchingNewParentComment && <LoadingSpinner key="isFetchingNewComment" center/>}
-    </ul>
-  );
-};
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll, false);
+  }
+
+  onScroll = () => {
+    const { scrollHeight, offsetHeight } = document.body;
+    const { scrollY } = window;
+    if (scrollHeight - (scrollY + offsetHeight) < 700) {
+      const { commentsMeta } = this.props;
+      if (commentsMeta && !commentsMeta.isFetchingComments && commentsMeta.nextPageUrl) {
+        this.fetchNextPage();
+      }
+    }
+  }
+
+  fetchNextPage = () => {
+    const { commentsMeta, fetchNextPageComments, postId } = this.props;
+    fetchNextPageComments(commentsMeta.nextPageUrl, postId);
+  }
+
+  render() {
+    const { commentsMeta } = this.props;
+
+    if (!commentsMeta || (commentsMeta.isFetchingComments && commentsMeta.ids.length === 0)) {
+      return <LoadingSpinner center/>;
+    }
+
+    return (
+      <ul className="CommentList">
+        {commentsMeta.ids.map(id => <CommentListItem commentId={id} key={id}/>)}
+        {commentsMeta.isFetchingComments && <LoadingSpinner key="isFetchingComments" center/>}
+        {commentsMeta.isFetchingNewParentComment && <LoadingSpinner key="isFetchingNewComment" center/>}
+      </ul>
+    );
+  }
+}
 
 const mapDispatchToProps = { fetchNextPageComments: fetchNextPageCommentsAction };
 
