170ca33a0a85fb24db33c7185ceb415e022c627b
Joel-An <rejoelve@gmail.com>
2019-02-20 03:43:38 +0900

    style: linter 옵션 해제

--- src/store/selectors/posts.js
+++ src/store/selectors/posts.js
@@ -2,6 +2,7 @@ export const selectPosts = state => state.entities.posts;
 
 export const selectPostById = (state, id) => selectPosts(state)[id];
 
+// eslint-disable-next-line max-len
 export const selectPostsMetaByCategory = (state, categoryId) => state.pagination.postsByCategory[categoryId];
 
 export const selectEditingPostById = (state, postId) => state.editing.posts[postId];
