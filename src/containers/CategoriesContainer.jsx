c80396fdde601ba3b79ca9fd28e72cef0ef9025c
Joel-An <rejoelve@gmail.com>
2019-02-19 19:52:28 +0900

    feat: propTypes 정보 추가

--- src/containers/CategoriesContainer.jsx
+++ src/containers/CategoriesContainer.jsx
@@ -1,16 +1,29 @@
 import { connect } from 'react-redux';
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { categoryPropType, categoryIdPropType } from './WithCategory';
 
 import { loadCategories as loadCategoriesAction } from '../store/actions/categories';
 import { getCategories, getParentCategoryIds } from '../store/selectors/categories';
 
+
+export const CategoriesContainerPropTypes = {
+  categories: PropTypes.objectOf(categoryPropType.type),
+  parentCategoryIds: PropTypes.arrayOf(categoryIdPropType),
+  loadCategories: PropTypes.func.isRequired,
+};
+
+export const CategoriesContainerDefaultProps = {
+  categories: {},
+  parentCategoryIds: [],
+};
+
 export default function withCategoriesContainer(ComposedComponent) {
   class CategoriesContainer extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-      };
-    }
+    static propTypes = CategoriesContainerPropTypes;
+
+    static defaultProps = CategoriesContainerDefaultProps;
 
     componentDidMount() {
       const { loadCategories } = this.props;
