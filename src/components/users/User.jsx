0df5cf79f1050a225c2434766192ca9a74ab87e3
Joel-An <rejoelve@gmail.com>
2019-02-19 18:42:30 +0900

    refactor: 변경된 WithUser 적용

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import withUserContainer from '../../containers/UserContainer';
+import PropTypes from 'prop-types';
+import withUser, { userPropType } from '../../containers/WithUser';
 
 const User = (props) => {
   const { user, renderUser } = props;
   return renderUser(user);
 };
 
-export default withUserContainer(User);
+User.propTypes = {
+  user: userPropType.type.isRequired,
+  renderUser: PropTypes.func.isRequired,
+};
+
+export default withUser(User);
