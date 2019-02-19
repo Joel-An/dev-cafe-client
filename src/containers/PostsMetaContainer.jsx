f616cd819b38315fad758826b5cd94f39c324ba2
Joel-An <rejoelve@gmail.com>
2019-02-20 03:05:47 +0900

    refactor: 컴포넌트 이름 변경

--- src/containers/PostsMetaContainer.jsx
+++ src/containers/PostsMetaContainer.jsx
@@ -1,12 +1,34 @@
 import { connect } from 'react-redux';
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { postIdPropType } from './PostContainer';
+import { categoryIdPropType } from './WithCategory';
 
 import { loadPosts as loadPostsAction } from '../store/actions/posts';
 import { updateLastVisitedCategory as updateLastVisitedCategoryAction } from '../store/actions/categories';
 import { selectPostsMetaByCategory } from '../store/selectors/posts';
 
-export default function withPostListContainer(ComposedComponent) {
-  class PostListContainer extends React.Component {
+
+export const PostsMetaContainerPropTypes = {
+  postsMeta: PropTypes.shape({
+    isFetchingNewPost: PropTypes.bool.isRequired,
+    isFetchingPosts: PropTypes.bool.isRequired,
+    ids: PropTypes.arrayOf(postIdPropType).isRequired,
+    pages: PropTypes.arrayOf(postIdPropType).isRequired,
+    nextPageUrl: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]).isRequired,
+  }),
+  categoryId: categoryIdPropType,
+  loadPosts: PropTypes.func,
+  updateLastVisitedCategory: PropTypes.func,
+};
+
+export const PostsMetaContainerDefaultProps = {
+  postsMeta: undefined,
+};
+
+export default function withPostsMetaContainer(ComposedComponent) {
+  class PostsMetaContainer extends React.Component {
     componentDidMount() {
       const { categoryId, loadPosts, updateLastVisitedCategory } = this.props;
 
@@ -46,5 +68,5 @@ export default function withPostListContainer(ComposedComponent) {
   };
 
 
-  return connect(mapStateToProps, mapDispatchToProps)(PostListContainer);
+  return connect(mapStateToProps, mapDispatchToProps)(PostsMetaContainer);
 }
