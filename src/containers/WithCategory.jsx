deea426d65968ed926e67a703d28171aef71fa10
Joel-An <rejoelve@gmail.com>
2019-02-19 19:01:13 +0900

    refactor: 컴포넌트 이름 변경

---
+++ src/containers/WithCategory.jsx
@@ -0,0 +1,27 @@
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { getCategoryById } from '../store/selectors/categories';
+
+const mapStateToProps = (state, ownProps) => {
+  const { categoryId } = ownProps;
+  const category = getCategoryById(state, categoryId);
+
+  return { category };
+};
+
+export default function withCategory(Component) {
+  return connect(mapStateToProps)(Component);
+}
+
+const categoryIdPropType = PropTypes.string;
+
+export const categoryPropType = {
+  type: PropTypes.shape({
+    _id: categoryIdPropType.isRequired,
+    parent: categoryIdPropType,
+    isChild: PropTypes.bool.isRequired,
+    name: PropTypes.string.isRequired,
+    children: PropTypes.arrayOf(categoryIdPropType),
+  }),
+};
