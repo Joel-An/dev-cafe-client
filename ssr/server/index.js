4f3cb4ee1f96a0804d987257590af70a68724a8a
Joel-An <rejoelve@gmail.com>
2019-03-03 22:50:03 +0900

    feat: 배포용 서버의 엔트리 파일로 변경
    server.js가 listen이 호출된 상태로 export되기 때문에 import만 함

--- ssr/server/index.js
+++ ssr/server/index.js
@@ -1,13 +1,2 @@
-/* eslint-disable import/first */
-/* eslint-disable no-console */
-import app from './server';
-
-const port = 8000;
-
-app.listen(port, (error) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
-  }
-});
+// eslint-disable-next-line no-unused-vars
+import server from './server';
