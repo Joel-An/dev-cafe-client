240bb7fd8e8168b83f2e92e1296d13a5af1c867b
Joel-An <rejoelve@gmail.com>
2019-03-03 16:46:11 +0900

    feat: 서버 렌더링 시, 포탈 렌더링 비활성화
    DOM이 없어서 에러 발생함

--- src/components/posts/PostListNavPortal.jsx
+++ src/components/posts/PostListNavPortal.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './PostListNavPortal.scss';
 
 const PostListNavPortal = ({ children }) => {
+  if (!process.env.BROWSER) {
+    return null;
+  }
   const el = document.getElementById('post-list-page-nav');
   return ReactDOM.createPortal(children, el);
 };
