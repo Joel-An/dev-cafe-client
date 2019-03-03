89104438ceda13588190e7eec4772b25ed2b9fc1
Joel-An <rejoelve@gmail.com>
2019-03-03 16:43:34 +0900

    feat: 서버 렌더링 시, 페이지마다 fetch할 데이터의 액션 추가.
    어떤 페이지인지 (pageAction), 페이지를 위해 fetch해야할 데이터는 무엇인지(fetchActions)를 전달해줘야
    
    서버에서 액션들을 디스패치하고, 완료되면 렌더링후 전송할 수 있다.
    
    코드가 좀 난잡한데 더 나은 방법이 생각이 안 난다.

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,53 +1,89 @@
+import UrlPattern from 'url-pattern';
+
 import Home from './Home';
 import NoMatch from './NoMatch';
 import SignupPage from './Signup';
 import ManageCategoriesPage from './categories/ManageCategoriesPage';
 
-import PostListPage from './posts/PostListPage';
+import PostListPage, { getCategoryId } from './posts/PostListPage';
 import PostPage from './posts/PostPage';
 import WritePostPage from './posts/Write';
 import EditPostPage from './posts/EditPostPage';
 import AdminPage from './AdminPage';
+
+import {
+  ssrCommonPage,
+  ssrPostPage,
+  ssrPostListPage,
+} from '../store/actions/ssr';
+
+import { loadCategories } from '../store/actions/categories';
+import { loadPost, loadPosts } from '../store/actions/posts';
+import { loadComments } from '../store/actions/comments';
+
+const defaultFetchActions = [loadCategories()];
+
+// eslint-disable-next-line max-len
+const setFetchActions = (pageAction = ssrCommonPage(), ...fetchActions) => [pageAction, ...defaultFetchActions, ...fetchActions];
 
 const Routes = [
   {
     path: '/',
     exact: true,
     component: Home,
+    mapUrlToActions: () => setFetchActions(),
   },
   {
-    path: '/posts/:id',
+    path: '/posts/:postId',
     component: PostPage,
+    mapUrlToActions: (url) => {
+      const pattern = new UrlPattern('/posts/:postId');
+      const { postId } = pattern.match(url);
+
+      return setFetchActions(ssrPostPage(), loadPost(postId), loadComments(postId));
+    },
   },
   {
     path: '/posts',
     component: PostListPage,
+    mapUrlToActions: (url) => {
+      // posts?category=all 이런 형태로 구성되어있음
+      const categoryId = getCategoryId(url);
+
+      return setFetchActions(ssrPostListPage(), loadPosts(categoryId));
+    },
   },
   {
     path: '/write/:categoryId',
     component: WritePostPage,
+    mapUrlToActions: () => setFetchActions(),
   },
   {
     path: '/edit/:id',
     component: EditPostPage,
+    mapUrlToActions: () => setFetchActions(),
   },
   {
     path: '/signup',
     exact: true,
     component: SignupPage,
+    mapUrlToActions: () => setFetchActions(),
+  },
+  {
+    path: '/admin',
+    exact: true,
+    component: AdminPage,
+    mapUrlToActions: () => setFetchActions(),
   },
   {
     path: '/categoryManager',
     exact: true,
-    component: AdminPage,
-  },
-  {
-    path: '/AdminPage',
-    exact: true,
     component: ManageCategoriesPage,
+    mapUrlToActions: () => setFetchActions(),
   },
   {
     component: NoMatch,
+    mapUrlToActions: () => setFetchActions(),
   },
 ];
 
