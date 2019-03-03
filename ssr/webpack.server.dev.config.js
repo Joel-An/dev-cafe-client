409fd3637b4b6ce913ebf21e065c1f39e72c67d2
Joel-An <rejoelve@gmail.com>
2019-03-03 16:53:46 +0900

    feat: 서버 빌드용 웹팩 config 추가

---
+++ ssr/webpack.server.dev.config.js
@@ -0,0 +1,63 @@
+/* eslint-disable */
+const webpack = require('webpack');
+const path = require('path');
+const webpackNodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'development',
+  target: 'node',
+  entry: {
+    server: [
+      '@babel/polyfill', 
+      './ssr/server/index.js'
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname,'build'),
+    filename: '[name].dev.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+               {
+                  targets: { node: 'current' },
+               },
+              ],
+              ['@babel/react']
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: 'ignore-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: 'ignore-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+    }),
+  ],
+  externals: [webpackNodeExternals({
+    whitelist: [/\.(?!(?:jsx?|json)$).{1,5}$/i],
+  })],
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.json', '.jsx', '.css'],
+  },
+};
