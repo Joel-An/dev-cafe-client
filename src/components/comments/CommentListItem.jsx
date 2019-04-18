b46b8e8b44013808cca58078f6c7793a3cda8aff
Joel-An <rejoelve@gmail.com>
2019-04-18 14:39:46 +0900

    feat: 댓글 목록 디자인 변경
    - 수정 버튼을 누르면 프사, 프로필 이름은 남아있고 내용만 수정화면으로 바뀌게 변경
    - 버튼들을 상단의 프사, 프로필 이름 표시하는 줄에 넣었음. 좀 더 깔끔해보여

--- src/components/comments/CommentListItem.jsx
+++ src/components/comments/CommentListItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import dateFormat from 'dateformat';
+import { Link } from 'react-router-dom';
 
 import withComment, { commentPropInfo } from '../../containers/WithComment';
 import withMyInfo, { myInfoPropType } from '../../containers/WithMyInfo';
@@ -10,34 +11,21 @@ import ChildCommentList from './ChildCommentList';
 import WriteComment from './WriteComment';
 import DeleteComment from './DeleteComment';
 import EditComment from './EditComment';
-import { Comment } from './Comment';
 import ContentsViewer from '../contents/Viewer';
 
 import User from '../users/User';
 
 import './CommentListItem.scss';
 
-const getProfileName = user => (<a href={`#/${user.username}`}>{user.profileName}</a>);
-const getProfilePic = user => (<div className="profile-pic" />);
+const getProfileName = user => (<Link to={`/users/${user._id}`}>{user.profileName}</Link>);
+const getProfilePic = user => (
+  <Link to={`/users/${user._id}`}>
+    <img alt={`@${user.profileName}`} height="44" width="44" src={user.profilePic}/>
+  </Link>
+);
 
 const ProfileName = ({ userId }) => <User userId={userId} renderUser={getProfileName}/>;
 const ProfilePic = ({ userId }) => <User userId={userId} renderUser={getProfilePic}/>;
-
-
-const renderComment = comment => (
-  <article className="comment">
-    <div className="comment-header">
-      <ProfilePic userId={comment.author}/>
-      <span className="comment-meta">
-        <ProfileName userId={comment.author}/>
-        <p className="date">
-          {dateFormat(comment.date, 'yy/mm/dd h:MM TT')}
-        </p>
-      </span>
-    </div>
-    <ContentsViewer className="comment-body markdown" contents={comment.contents}/>
-  </article>
-);
 
 class CommentListItem extends React.Component {
   constructor(props) {
@@ -66,23 +54,33 @@ class CommentListItem extends React.Component {
 
     return (
       <li key={comment._id} className="CommentListItem">
-        {isEditing
-          ? <EditComment commentId={comment._id} offEditMode={this.onEdit}/>
-          : <Comment comment={comment} renderComment={renderComment}/>
-        }
-        <div className="comment-button-group">
-          { isMyComment
-          && <>
-            <button type="button" onClick={this.onEdit}>
-              {isEditing ? '취소' : '수정'}
-            </button>
-            <DeleteComment commentId={comment._id}/>
-          </> }
-          {comment.isChild || <button type="button" onClick={this.onAddReply}>
-          답글
-          </button>
+        <article className="comment">
+          <div className="comment-header">
+            <ProfilePic userId={comment.author}/>
+            <span className="comment-meta">
+              <ProfileName userId={comment.author}/>
+              <p className="date">
+                {dateFormat(comment.date, 'yy/mm/dd h:MM TT')}
+              </p>
+            </span>
+            <div className="comment-control-group">
+              { isMyComment
+            && <>
+              <button type="button" onClick={this.onEdit}>
+                {isEditing ? '취소' : '수정'}
+              </button>
+              <DeleteComment commentId={comment._id}/>
+              </>
+              }
+              {comment.isChild || <button type="button" onClick={this.onAddReply}>답글</button>}
+            </div>
+          </div>
+          {isEditing
+            ? <EditComment commentId={comment._id} offEditMode={this.onEdit}/>
+            : <ContentsViewer className="comment-body markdown" contents={comment.contents}/>
           }
-        </div>
+        </article>
+
         <ChildCommentList childCommentIds={comment.childComments}/>
         {isFetchingNewChildComment && <LoadingSpinner center/> }
         {!addReply || <div className="addReply">
