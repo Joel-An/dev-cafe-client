353907e0e7c5bd3cd84c6e87cf1f4c544875ee93
Joel-An <rejoelve@gmail.com>
2019-04-23 01:36:30 +0900

    feat: 글쓰기 버튼에 button 대신 a tag만 사용하도록 변경
    버튼 태그를 전부 제거할 예정

--- src/components/posts/WritePostButton.jsx
+++ src/components/posts/WritePostButton.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import withLastVisitedCategoryId, { lastVisitedCategoryIdPropType } from '../../containers/WithLastVisitedCategoryId';
 
 const WritePostButton = (props) => {
   const { lastVisitedCategoryId } = props;
   return (
     <Link to={`/write/${lastVisitedCategoryId}`}>
-      <button type="button">
-        글쓰기
-      </button>
+      글쓰기
     </Link>
   );
 };
