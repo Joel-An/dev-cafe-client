097575d1680dda9b2400e27eb772d84c386cc014
Joel-An <rejoelve@gmail.com>
2019-04-23 01:35:08 +0900

    feat: comment id로 post author id를 찾는 selector 추가
    댓글 목록은 postId만으로 출력되기때문에 글 작성자 정보를 바로 알수가 없음
    
    글 작성자가 댓글에 하트를 주려면 댓글 목록에서 글 작성자 정보를 알아야해서 selector를 만들었음

--- src/store/selectors/posts.js
+++ src/store/selectors/posts.js
@@ -1,3 +1,5 @@
+import { selectCommentById } from './comments';
+
 export const selectPosts = state => state.entities.posts;
 
 export const selectPostById = (state, id) => selectPosts(state)[id];
@@ -6,3 +8,13 @@ export const selectPostById = (state, id) => selectPosts(state)[id];
 export const selectPostsMetaByCategory = (state, categoryId) => state.pagination.postsByCategory[categoryId];
 
 export const selectEditingPostById = (state, postId) => state.editing.posts[postId];
+
+export const selectPostAuthorIdByCommentId = (state, commentId) => {
+  const comment = selectCommentById(state, commentId);
+
+  if (!comment) return null;
+
+  const post = selectPostById(state, comment.post);
+
+  return post ? post.author : null;
+};
