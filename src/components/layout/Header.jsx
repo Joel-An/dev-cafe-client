bf0f03112f6a5bc1aeb2d1d0cfef1f14da0a7626
Joel-An <rejoelve@gmail.com>
2019-04-24 23:38:31 +0900

    feat: Header 컴포넌트에서 로그인,myInfo 관련 로직을 제거
    saga와 로그인 팝업에서 처리하도록 바꿈

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,65 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-
-import { fetchMyInfo as fetchMyInfoAction } from '../../store/actions/auth';
-
-import withToken, { tokenPropType } from '../../containers/WithToken';
-import { connectComponent } from '../../utils';
 
 import HeaderMenu from './HeaderMenu';
 
 import './Header.scss';
 
-class Header extends React.Component {
-  componentDidMount() {
-    const { token, fetchMyInfo } = this.props;
-    if (token) {
-      fetchMyInfo(token);
-    }
-  }
+const Header = () => (
+  <header className="main-head">
+    <div className="spacer"/>
+    <h1 className="title">
+      <Link to="/">Joel&apos;s Dev Cafe</Link>
+    </h1>
+    <HeaderMenu/>
+  </header>
+);
 
-  componentWillReceiveProps(nextProps) {
-    const { token, fetchMyInfo } = this.props;
-    if (nextProps.token && nextProps.token !== token) {
-      fetchMyInfo(nextProps.token);
-    }
-  }
-
-  render() {
-    const { profileName } = this.props;
-    return (
-      <header className="main-head">
-        <div className="spacer"/>
-        <h1 className="title">
-          <Link to="/">Joel&apos;s Dev Cafe</Link>
-        </h1>
-        <HeaderMenu/>
-      </header>
-    );
-  }
-}
-
-const mapStateToProps = ({ auth }) => ({
-  profileName: auth.user.profileName,
-});
-
-const mapDispatchToProps = { fetchMyInfo: fetchMyInfoAction };
-
-Header.propTypes = {
-  token: tokenPropType.type,
-  profileName: PropTypes.string,
-  fetchMyInfo: PropTypes.func.isRequired,
-};
-
-Header.defaultProps = {
-  token: tokenPropType.default,
-  profileName: null,
-};
-
-export default connectComponent(Header,
-  [
-    connect(mapStateToProps, mapDispatchToProps),
-    withToken,
-  ]);
+export default Header;
