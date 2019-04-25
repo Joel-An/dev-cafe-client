4c97938122f23c088a8ba2f005ee9a506f2965fe
Joel-An <rejoelve@gmail.com>
2019-04-26 00:59:32 +0900

    feat: 마이페이지 프사 추가, 로그아웃하면 리다이렉트

--- src/components/users/MyPage.jsx
+++ src/components/users/MyPage.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 import withMyInfo, { myInfoPropType } from '../../containers/WithMyInfo';
+import withLastVisitedCategoryId, { lastVisitedCategoryIdPropType } from '../../containers/WithLastVisitedCategoryId';
 import { connectComponent } from '../../utils';
 import UploadImage from '../posts/UploadImage';
 
 const MyPage = (props) => {
-  const { myInfo } = props;
+  const { myInfo, lastVisitedCategoryId } = props;
+
+  if (myInfo.isGuest) {
+    return <Redirect to={`/posts?category=${lastVisitedCategoryId}`} />;
+  }
 
   return (
     <div className="MyPage">
       마이 페이지!
       <br/>
-      {myInfo.profileName}!!
+      <img alt={`@${myInfo.profileName}`} height="200" width="200" src={myInfo.profilePic}/>
       <UploadImage path="profilePic">
         프사 변경!
       </UploadImage>
@@ -21,9 +27,11 @@ const MyPage = (props) => {
 
 MyPage.propTypes = {
   myInfo: myInfoPropType.type.isRequired,
+  lastVisitedCategoryId: lastVisitedCategoryIdPropType.isRequired,
 };
 
 export default connectComponent(MyPage,
   [
     withMyInfo,
+    withLastVisitedCategoryId,
   ]);
