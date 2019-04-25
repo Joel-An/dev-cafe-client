88598f81e0735b6264fdc8e885306d6e8de4365d
Joel-An <rejoelve@gmail.com>
2019-04-25 18:23:59 +0900

    feat: 로그인 안 했을 때 myInfo의 기본값 설정
    myInfo를 그냥 undefined로 하니까 기존 코드랑 충돌도 일어나고 깔끔하게 해결이 안 돼서
    isGuest 플래그를 추가한 기본 객체를 리턴하도록 변경함

--- src/store/selectors/auth.js
+++ src/store/selectors/auth.js
@@ -4,9 +4,15 @@ export const selectToken = state => state.auth.token;
 
 const selectMyId = state => state.auth.myId;
 
+const GUEST = {
+  _id: undefined,
+  profileName: undefined,
+  isGuest: true,
+};
+
 export const selectMyInfo = (state) => {
   const myId = selectMyId(state);
   const myInfo = selectUserById(state, myId);
 
-  return myInfo;
+  return myInfo || GUEST;
 };
